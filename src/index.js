cee4b7b408346dc31fb7628539e466430f3ce477
Jessica Stokes <hello@jessicastokes.net>
2016-11-20 11:46:15 +1100

    🖌 Simplify main entry point

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-import unitConstructor from './unit';
+export { default as Unit } from './unit';
 
-export const Unit = unitConstructor;
-
-const { default: dataConstructor, ...dataUnits } = require('./data');
-
-export const Data = dataConstructor;
-export const data = dataUnits;
-
-const { default: durationConstructor, ...durationUnits } = require('./duration');
-
-export const Duration = durationConstructor;
-export const duration = durationUnits;
-
-const { default: temperatureConstructor, ...temperatureUnits } = require('./temperature');
-
-export const Temperature = temperatureConstructor;
-export const temperature = temperatureUnits;
+export const { default: Data, ...data } = require('./data');
+export const { default: Duration, ...duration } = require('./duration');
+export const { default: Temperature, ...temperature } = require('./temperature');
